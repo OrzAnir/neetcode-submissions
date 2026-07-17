26717486c45094416dc3d222df07461f033e7a82
Aniketa R <77120521+OrzAnir@users.noreply.github.com>
2026-07-17 11:14:36 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int l1 = s1.length(), l2 = s2.length();
+        sort(s1.begin(), s1.end());
+        for(int i = 0; i < l2 - l1 + 1; i++) {
+            string str = s2.substr(i, l1);
+            sort(str.begin(), str.end());
+            if (str == s1) return true;
+        }
+        return false;
+    }
+};
