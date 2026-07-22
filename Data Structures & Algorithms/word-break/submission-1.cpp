45b16f7b4643cf5e5f222400ab0fd40fc4ccb2b8
Aniketa R <77120521+OrzAnir@users.noreply.github.com>
2026-07-22 05:50:50 +0530

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool func(int index, string& s, set<string>& st, vector<int>& dp) {
+        int n = s.size();
+        if (index >= n) return true;
+        if (dp[index] != -1) return dp[index];
+        for(int i = index; i < n; i++) {
+            string str = s.substr(index, i - index + 1);
+            if (st.find(str) != st.end()) {
+                if (func(i + 1, s, st, dp)) return dp[index] = true;
+            }
+        }
+        return dp[index] = false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        set<string> st;
+        int n = s.size();
+        for(string s1 : wordDict) st.insert(s1);
+        vector<int> dp(n, -1);
+        return func(0, s, st, dp);
+    }
+};
